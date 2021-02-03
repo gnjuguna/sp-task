c0d0f01a1aaefbab4a7a301a3459b821baaa8960
George Njuguna <gnjuguna83@gmail.com>
2021-02-04 00:21:56 +0300

    initial set up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { AlertModule } from 'ngx-bootstrap/alert';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
+    RouterModule,
+    AlertModule.forRoot(),
+    BsDropdownModule.forRoot(),
     AppRoutingModule
   ],
   providers: [],
