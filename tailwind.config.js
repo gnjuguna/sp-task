d95e84824c599905bc61b589a2a7e91ac2183fb5
George Njuguna <gnjuguna83@gmail.com>
2021-02-05 09:09:30 +0300

    change text size add border on accordian header

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,13 @@ module.exports = {
   purge: ['./src/**/*.{html,ts}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        '2xs': '.65rem',
+        '3xs': '.50rem',
+        '4xs': '.40rem'
+      }
+    },
   },
   variants: {
     extend: {},
